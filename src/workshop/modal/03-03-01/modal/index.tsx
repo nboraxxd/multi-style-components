90d80d6607615a68b6b4c403476f3f9ffbe09fae
bora <nhatbaoxxd@gmail.com>
2023-12-21 09:50:17 +0700

    Exercise 03-03. Update the modal to support dynamic contents

--- src/workshop/modal/03-03-01/modal/index.tsx
+++ src/workshop/modal/03-03-01/modal/index.tsx
@@ -14,7 +14,26 @@ export default function ModalDemo() {
       and `action` props to the modal below.
       ------------------------------
     */}
-      <Modal open={isOpen} onClose={() => setIsOpen(false)} />
+      <Modal
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        title="Confirm subscription"
+        actions={{
+          confirm: { action: () => setIsOpen(false), label: 'Confirm' },
+          cancel: { action: () => setIsOpen(false), label: 'Cancel' },
+        }}
+      >
+        {/* Panel body */}
+        <div className="mt-4">
+          <p className="text-slate-500">
+            You're about to confirm your{' '}
+            <a className="text-indigo-600 underline hover:text-indigo-500" href="#">
+              membership subscription
+            </a>
+            . Your account will be billed for a one-year membership. We just want to make sure you understand that.
+          </p>
+        </div>
+      </Modal>
 
       <Button impact="light" onClick={() => setIsOpen(true)}>
         Open dialog
